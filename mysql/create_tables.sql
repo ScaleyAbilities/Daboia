a0f22a781963a38ceca88b7edc507dbd0665b840
wyll <wyll.brimacombe@gmail.com>
2019-02-01 18:56:11 -0800

    create remakes tables now

--- mysql/create_tables.sql
+++ mysql/create_tables.sql
@@ -1,5 +1,13 @@
 use scaley_abilities
 
+-- source mysql/drop_tables.sql
+
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS stocks;
+DROP TABLE IF EXISTS logs;
+DROP TABLE IF EXISTS logs_work;
+DROP TABLE IF EXISTS users;
+
 -- sudo docker-compose up database
 -- mysql -h 127.0.0.1 -u scaley -p
 -- -> source mysql/create.sql
